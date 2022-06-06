faca499e9c12bd4b5190e1a103b7aa2df95f7a75
Pablo Cha <pablocha1992@gmail.com>
2022-06-06 13:50:36 +0200

    chore(version): support latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.jetbrains.intellij") version "1.4.0"
     kotlin("jvm") version "1.5.10"
     java
-    id("org.jetbrains.grammarkit") version "2021.2.1"
+    id("org.jetbrains.grammarkit") version "2021.2.2"
 }
 
 group = "org.pcha"
@@ -20,7 +20,7 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2021.3.2")
+    version.set("2022.1.2")
 }
 tasks {
 
